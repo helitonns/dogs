c15428408f6efeb7cfc5dcaeb5ac83bb55991b7c
heliton <helitonns@gmail.com>
2024-01-17 09:56:42 -0400

    Login routes

---
+++ src/Components/Login/LoginPasswordLost.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const LoginPasswordLost = () => {
+  return <div>Login password lost</div>;
+};
+
+export default LoginPasswordLost;
---
+++ src/Components/Login/LoginPasswordReset.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const LoginPasswordReset = () => {
+  return <div>login password reset</div>;
+};
+
+export default LoginPasswordReset;
