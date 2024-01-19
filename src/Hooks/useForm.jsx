e9d3d45e3056f3d23bceac3b651f193e2c6f1286
heliton <helitonns@gmail.com>
2024-01-19 12:15:52 -0400

    Foto post

--- src/Hooks/useForm.jsx
+++ src/Hooks/useForm.jsx
@@ -6,6 +6,11 @@ const types = {
       /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
     message: "Preencha um e-mail válido",
   },
+  number :{
+    regex:
+      /^\d+$/,
+      message: "Utilize números apenas",
+  }
 };
 
 const useForm = (type) => {
