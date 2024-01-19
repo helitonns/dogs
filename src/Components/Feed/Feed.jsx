390ce0f7ae126d78f17853687acd2ea4010121c4
heliton <helitonns@gmail.com>
2024-01-19 12:48:22 -0400

    Fazendo Feed

--- src/Components/Feed/Feed.jsx
+++ src/Components/Feed/Feed.jsx
@@ -1,7 +1,14 @@
 import React from "react";
+import FeedModal from "./FeedModal";
+import FeedPhoto from './FeedPhotos';
 
 const Feed = () => {
-  return <div>Feed</div>;
+  return (
+  <div>
+    <FeedModal />
+    <FeedPhoto />
+  </div>
+  );
 };
 
 export default Feed;
