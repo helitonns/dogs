a29b527b259c0ba14824a7454d9d05ecf5e7a8cb
heliton <helitonns@gmail.com>
2024-01-22 09:54:07 -0400

    Perdeu senha 1

--- src/Components/Login/LoginForm.jsx
+++ src/Components/Login/LoginForm.jsx
@@ -36,7 +36,7 @@ const LoginForm = () => {
 
         <Error error={error} /> 
       </form>
-      <Link className={styles.perdeu} to="/login/perder">
+      <Link className={styles.perdeu} to="/login/perdeu">
         Perdeu a senha?
       </Link>
       <div className={styles.cadastro}>
--- src/Components/Login/LoginPasswordLost.jsx
+++ src/Components/Login/LoginPasswordLost.jsx
@@ -1,7 +1,38 @@
 import React from "react";
+import Input from './../Forms/Input';
+import Button from './../Forms/Button';
+import useForm from './../../Hooks/useForm';
+import useFetch from './../../Hooks/useFetch';
+import { PASSWORD_LOST } from "../../api";
+import Error from './../Helper/Error';
 
 const LoginPasswordLost = () => {
-  return <div>Login password lost</div>;
+  const login = useForm();
+  const {data, loading, error, request} = useFetch();
+
+  async function handleSubmit(event){
+    event.preventDefault();
+    
+    if(login.validate()){
+      const {url, options} = PASSWORD_LOST({login: login.value, url: "http://http://localhost:5173/login/resetar"});
+      request(url, options);
+    }
+  }
+
+  return (
+    <section>
+      <h1 className="title">Perdeu a senha?</h1>
+      {data ? 
+        <p style={{color: "#4c1", fontWeight: "bold"}}>{data}</p> 
+        :
+        <form onSubmit={handleSubmit}>
+          <Input label="E-mial / Usuário" type="text" name="email" {...login}/>
+          {loading ? <Button disabled>Enviando...</Button> : <Button>Enviar E-mail</Button>}
+        </form>
+      }
+      <Error error={error} />
+    </section>
+  );
 };
 
 export default LoginPasswordLost;
