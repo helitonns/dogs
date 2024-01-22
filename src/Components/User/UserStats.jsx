4981d1f213cfc56cd332ef0fb02259d42ae7c21e
heliton <helitonns@gmail.com>
2024-01-22 11:42:30 -0400

    Biblioteca externa

--- src/Components/User/UserStats.jsx
+++ src/Components/User/UserStats.jsx
@@ -1,10 +1,32 @@
 import React from "react";
 import Head from './../Helper/Head';
+import useFetch from "../../Hooks/useFetch";
+import { STATS_GET } from './../../api';
+import Loading from './../Helper/Loading';
+import Error from './../Helper/Error';
+import UserStatsGraphs from "./UserStatsGraphs";
 
 const UserStats = () => {
-  return <div>
-    <Head title="Estatísticas" description="Home do site Dogs" />
-    UserStats</div>;
+  const {data, loading, error, request} = useFetch();
+
+  React.useEffect(()=> {
+    async function getData(){
+      const {url, options} = STATS_GET();
+      await request(url, options);
+    }
+    getData();
+  }, [request]);
+
+  if(loading) return <Loading />
+  if(error) return <Error error={error} />
+  if(data)
+  return( 
+    <div>
+      <Head title="Estatísticas" description="Home do site Dogs" />
+      <UserStatsGraphs data={data} />
+    </div>
+  );
+  else return null;
 };
 
 export default UserStats;
